675f77fca1c6661f5ba848c922d64f925b5714e5
dipak-kumawat <dipaksk11@gmail.com>
2024-01-23 17:54:16 +0530

    vector function

--- dynamicArray.cpp
+++ dynamicArray.cpp
@@ -1,27 +1,8 @@
-#include <iostream>
-#include <vector>
+#include <stdio.h>
 using namespace std;
 
-void print(vector<int>v){
-    int size = v.size();
-    cout<<"size of array is "<<size<<endl;
-    for (int i = 0; i < size; i++)
-    {
-        cout<<v[i]<<endl;
-
-    }
-    
-}
-
 int main(){
-
-    // use of vector function in array 
-vector<int>v;
-v.push_back(12);
-v.push_back(12);
-v.push_back(12);
-v.push_back(12);
-
-print(v);
+//  static type of array 
+int arr[5] = {1, 2, 3, 4, 5};
 
 }
