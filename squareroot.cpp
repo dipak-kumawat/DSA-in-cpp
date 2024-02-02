eb74409cc9d72fca61d24fca32d72850ec03bdaf
dipak-kumawat <dipaksk11@gmail.com>
2024-02-02 22:19:45 +0530

    square root found

--- squareroot.cpp
+++ squareroot.cpp
@@ -2,38 +2,41 @@
 #include <vector>
 using namespace std;
 
-int squareRoot(int number){
-      int left = 0;
-      int right = number - 1;
-      int i = 0;
-      int ans = -1;
-      int mid = left + (left - right)/2;
+int squareRoot(int number)
+{
+    int left = 0;
+    int right = number;
+    int i = 0;
+    int ans = -1;
+    int mid = left + (left - right) / 2;
 
-      while (left <= right)
-      {
-        if ( mid * mid  == number  ) {
+    while (left <= right)
+    {
+        if (mid * mid == number)
+        {
             return mid;
-        }else if ( mid * mid  > number){
-            right = mid -1;
-        }else if ( mid * mid  < number ){
-            ans = mid;
-            left = mid +1;
-        }else if ( left = right ){
-            return left;
         }
-        mid = left + (left - right)/2;
-      }
-      return ans;
-
-      
+        else if (mid * mid < number)
+        {
+            ans = mid;
+            left = mid + 1;
+        }
+        else //( mid * mid  > number)
+        {
+            right = mid - 1;
+        }
+        mid = left + (left - right) / 2;
+    }
+    return ans;
 }
 
-int main(){
+int main()
+{
 
     int number = 64;
     int n = squareRoot(number);
 
-    cout << " squareroot of: "<< n <<endl;
+    cout << " squareroot of: " << n << endl;
 
     return 0;
 }
