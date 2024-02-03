930e140376fa7f57e2406fc5c6c3db10d3afaaaa
dipak-kumawat <dipaksk11@gmail.com>
2024-02-03 08:23:17 +0530

    square root

--- squareroot.cpp
+++ squareroot.cpp
@@ -2,39 +2,103 @@
 #include <vector>
 using namespace std;
 
-int squareRoot(int number)
+int squareRoot(int x)
 {
-    int left = 0;
-    int right = number;
-    int i = 0;
+    int s = 0;
+    int e = x;
+    long long int mid = s + (s-e)/2;
     int ans = -1;
-    int mid = left + (left - right) / 2;
 
-    while (left <= right)
+    while (s <= e)
     {
-        if (mid * mid == number)
+        if (mid*mid == x)
         {
             return mid;
         }
-        else if (mid * mid < number)
+        else if(mid*mid < x)
         {
             ans = mid;
-            left = mid + 1;
+            s = mid+1;
         }
         else //( mid * mid  > number)
         {
-            right = mid - 1;
+            e = mid-1;
         }
-        mid = left + (left - right) / 2;
+        mid = s + (s-e)/2;
     }
     return ans;
 }
 
+int mySqrt(int x) {
+        int s = 0;
+        int e = x;
+        long long int mid = s + (e-s)/2;
+        int ans = -1;
+
+        while(s <= e) {
+            //kya mid hi toh answer nahi
+            if(mid*mid == x) {
+                return mid;
+            }
+            else if(mid*mid < x) {
+                //ans store
+                //right me jao
+                ans = mid;
+                s = mid+1;
+            }
+            else {
+                //left me jana h 
+                e = mid-1;
+            }
+            mid = s + (e-s)/2;
+        }
+        return ans;
+    }
+
+int newsquare(int x){
+    int s = 0;
+    int e =x;
+    int ans = -1;
+
+    while(s<=e){
+    long  int mid = s + (e-s)/2;
+    int val = mid*mid;
+        if(val == x) {
+            return mid;
+        }else if(val < x) {
+            ans = mid;
+            s =mid+1;
+        }else {
+            e = mid- 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+int sqare(int n){
+       int low = 1;
+        int high = n;
+        while (low<=high){
+            long mid = low +(high-low)/2;
+            long val = mid*mid;
+            if(val>(long)(n)){
+                high =(int) mid-1;
+            }
+            else {
+                low =(int) mid +1 ;
+            }
+        }
+        return low-1;
+    }
+
+
 int main()
 {
 
-    int number = 64;
-    int n = squareRoot(number);
+    int number = 50;
+    int n = newsquare(60);
+
 
     cout << " squareroot of: " << n << endl;
 
