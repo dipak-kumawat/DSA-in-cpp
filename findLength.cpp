6453716d730aace5145ca1478e764b004f9ee107
dipak-kumawat <dipaksk11@gmail.com>
2024-02-05 22:41:52 +0530

    find length

---
+++ findLength.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int length(char n[], int size)
+{
+    int i = 0;
+    int len = 0;
+    while (n[i] != '\0')
+    {
+        /* code */
+        len++;
+        i++;
+    }
+    return len;
+}
+
+int main()
+{
+    char n[100];
+    cout << "enter your name: ";
+    cin >> n;
+    cout << "your name is : " << n<<endl;
+    cout << "length of your name is " << length(n, 100);
+    return 0;
+}
